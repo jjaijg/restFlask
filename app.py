4a48a36cdb8b512e0f4ba1d7744ec43da3473d60
jjaijg <jjaiganeshjg95@gmail.com>
2021-07-23 01:10:24 +0530

    added CORS for the app

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_restful import Api
 from flask_jwt import JWT
+from flask_cors import CORS
 
 from db import db
 from resources.user import UserRegister
@@ -12,6 +13,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'My secret key'
+CORS(app, resources={
+     r"/*": {"origins": ["https://flaskstore.azurewebsites.net"]}})
 api = Api(app)
 
 
